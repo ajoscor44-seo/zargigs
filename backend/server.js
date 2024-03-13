12233d6d62d42993af5d86e3b4551f6d62a23ee6
Rabiu Ahmad <rabiu2993@gmail.com>
2024-03-13 16:14:53 -0700

    Connected to db

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 // import bodyParser from "body-parser";
 // import cors from "cors";
-// import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
+
+app.listen(3000, () => {
+  console.log("Server running");
+});
 
 // app.use(bodyParser.json({ limit: "30mb", extended: true }));
 // app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
