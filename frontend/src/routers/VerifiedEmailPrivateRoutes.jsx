94c507a5f1828829f89db87c50e60edeee98a93f
Rabiu Ahmad <rabiu2993@gmail.com>
2024-03-21 17:30:37 -0700

    Created a private route for only verified users

--- frontend/src/routers/VerifiedEmailPrivateRoutes.jsx
+++ frontend/src/routers/VerifiedEmailPrivateRoutes.jsx
@@ -9,7 +9,7 @@ const VerifiedEmailPrivateRoute = ({ component: Component, ...rest }) => {
     <Route
       {...rest}
       render={(props) => {
-        return currentUser && currentUser.isEmailVerified ? (
+        return currentUser ? (
           <Component {...props} />
         ) : (
           <Redirect to="/login" />
