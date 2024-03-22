4ad1dc690b79f4cfafd629d69cde79d60791f4fb
Rabiu Ahmad <rabiu2993@gmail.com>
2024-03-21 17:30:06 -0700

    Created a private route for Pages after signup (Verify Email Page)

--- frontend/src/routers/PrivateRoutes.jsx
+++ frontend/src/routers/PrivateRoutes.jsx
@@ -4,14 +4,15 @@ import { useAuth } from "../context/AuthContext";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const { currentUser } = useAuth();
+
   return (
     <Route
       {...rest}
       render={(props) => {
-        return currentUser && !currentUser.isEmailVerified ? (
+        return (currentUser && !currentUser.isEmailVerified) || !currentUser ? (
           <Component {...props} />
         ) : currentUser && currentUser.isEmailVerified ? (
-          <Redirect to="/set-location" />
+          <Redirect to="/dashboard" />
         ) : (
           <Redirect to="/login" />
         );
