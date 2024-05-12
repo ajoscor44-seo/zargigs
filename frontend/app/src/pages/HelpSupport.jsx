6b0481559a3d20a9af125bdbc14e626384f4c429
Rabiu Ahmad <rabiu2993@gmail.com>
2024-05-12 11:37:47 -0700

    Modal fix

--- frontend/app/src/pages/HelpSupport.jsx
+++ frontend/app/src/pages/HelpSupport.jsx
@@ -6,7 +6,8 @@ import Supports from "../components/Supports/Supports";
 import ClientMenuBar from "../components/ClientMenuBar/ClientMenuBar";
 import Chat from "../components/Chat/Chat";
 import axios from "axios";
-import { Alert, Button, Modal, Spinner } from "react-bootstrap";
+import { IoClose } from "react-icons/io5";
+import { FaSpinner } from "react-icons/fa6";
 
 const HelpSupport = () => {
   const disclaimerMsg =
@@ -14,15 +15,34 @@ const HelpSupport = () => {
 
   const [visible, setVisibility] = useState(false);
   const [isAdding, setIsAdding] = useState(false);
-  const [error, setError] = useState(false);
-  const [complaint, setcomplaint] = useState({});
+  const [error, setError] = useState(null);
+  const [complaint, setComplaint] = useState({
+    complaint: "",
+    proof: "",
+  });
 
   const postComplaint = async () => {
     try {
+      setIsAdding(true);
+      if (!complaint.complaint) {
+        setIsAdding(false);
+        return setError("Please input a complaint.");
+      }
+
+      setError(null);
       const response = await axios.post("/api/v1/admin/complaints", complaint);
+
+      if (response.data.failed) {
+        setIsAdding(false);
+        return setError(response.data.message);
+      }
+
+      setError(null);
+      setIsAdding(false);
+      return setVisibility(false);
     } catch (error) {
       setIsAdding(false);
-      return setError(error);
+      return setError("Oops an error occurred");
     }
   };
 
@@ -38,40 +58,74 @@ const HelpSupport = () => {
         <Supports />
         <Chat handleVisibility={handleVisibility} />
       </div>
-      <Modal
-        show={visible}
-        onHide={() => handleVisibility(false)}
-        backdrop="static"
-        keyboard={false}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>Having A Problem With Gigsflix?</Modal.Title>
-        </Modal.Header>
-        <Modal.Header>
-          {error && (
-            <Alert color="red" className="w-100 text-center">
-              {error}
-            </Alert>
-          )}
-        </Modal.Header>
-        <Modal.Body>Hello</Modal.Body>
-        <Modal.Footer>
-          <Button
-            variant="secondary"
-            onClick={() => handleVisibility(false)}
-            disabled={isAdding}
-          >
-            Close
-          </Button>
-          <button
-            className={"here_comes-the_class"}
-            onClick={() => postComplaint()}
-            disabled={isAdding}
-          >
-            {isAdding ? <Spinner /> : "Complain"}
-          </button>
-        </Modal.Footer>
-      </Modal>
+      <div className="px-3">
+        <div
+          className={`bg-dark modal_bg ${
+            visible ? "" : "hidden"
+          } fixed top-0 left-0 min-h-screen w-screen flex justify-center items-center`}
+        >
+          <div className="flex flex-col bg-white items-center justify-center p-3 rounded">
+            <div
+              className="flex items-center gap-10 justify-between"
+              style={{ maxWidth: "400px" }}
+            >
+              <h1 className="font-bold italic">
+                Having Any Problem With Gigsflix?
+              </h1>
+              <span
+                disabled={isAdding}
+                onClick={handleVisibility}
+                className="cursor-pointer border-2 border-white hover:border-gray-200"
+              >
+                <IoClose size={25} />
+              </span>
+            </div>
+
+            {error && (
+              <p className="font-semibold bg-red-200 w-full text-center py-1 rounded">
+                {error}
+              </p>
+            )}
+            <div>
+              <p className="text-xs font-bold my-2 text-gray-500">
+                Please, input your complaint in the space provided below.
+              </p>
+              <textarea
+                onChange={(e) =>
+                  setComplaint({
+                    ...complaint,
+                    complaint: e.target.value,
+                  })
+                }
+                placeholder="Input your issue here."
+                className="border p-2 border-gray-300 outline-none rounded w-full"
+                rows={5}
+              ></textarea>
+            </div>
+
+            <div className="mt-2 flex justify-end w-full gap-3">
+              <button
+                disabled={isAdding}
+                onClick={handleVisibility}
+                className="px-3 py-1 bg-red-500 text-white rounded"
+              >
+                Close
+              </button>
+              <button
+                disabled={isAdding}
+                onClick={postComplaint}
+                className="px-3 py-1 bg-green-500 text-white rounded"
+              >
+                {isAdding ? (
+                  <FaSpinner size="15" variant="white" />
+                ) : (
+                  "Complain"
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
       <ClientMenuBar />
     </div>
   );
