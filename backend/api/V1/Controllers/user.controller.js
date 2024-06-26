7f4f71e51c2a4e5a6a78377033815110005a72a0
justlogicalahmad <rabiu2993@gmail.com>
2024-06-26 10:19:42 -0700

    Fix become a member method to use wallet instead of payment

--- backend/api/V1/Controllers/user.controller.js
+++ backend/api/V1/Controllers/user.controller.js
@@ -1,4 +1,3 @@
-import userDetails from "../../V1/Models/user-details.model.js";
 import UserDetails from "../../V1/Models/user-details.model.js";
 import Admin from "../Models/admin.model.js";
 import User from "../Models/user.model.js";
@@ -37,41 +36,75 @@ export const getUserDetails = async (req, res, next) => {
 };
 
 export const addUserDetails = async (req, res, next) => {
-  const {
-    location,
-    religion,
-    gender,
-    dateOfBirth,
-    image,
-    bankDetails,
-    userEarnings,
-  } = req.body;
-
-  // Checks for valid user
-  const validUser = await User.findOne({ email: req.user.email });
-  if (!validUser) {
-    const error = ErrorHandler(404, "There's no user with this email.");
-    return res.status(404).json(error);
-  }
-  let referrer = await User.findOne({ username: validUser.referredBy });
-  //If NO referrer makes an admin referrer
-  if (!referrer) referrer = await User.findOne({ role: "admin" });
-  let referrerDetails = await UserDetails.findOne({ userId: referrer._id });
-  if (!referrerDetails) {
-    referrer = await User.findOne({ role: "admin" });
-    referrerDetails = await UserDetails.findOne({ userId: referrer._id });
-  }
-  const adminDatas = await Admin.find();
-  const adminData = adminDatas[0];
-
-  // Checks for user details
-  const userDetails = await UserDetails.findOne({ userId: validUser._id });
-  if (userDetails) {
-    const error = ErrorHandler(400, "User's details already exists.");
-    return res.status(400).json(error);
-  }
-
   try {
+    const {
+      location,
+      religion,
+      gender,
+      dateOfBirth,
+      image,
+      bankDetails,
+      userEarnings,
+    } = req.body;
+
+    // Checks for valid user
+    const validUser = await User.findOne({ email: req.user.email });
+    if (!validUser) {
+      const error = ErrorHandler(404, "There's no user with this email.");
+      return res.status(404).json(error);
+    }
+    let referrer = await User.findOne({ username: validUser.referredBy });
+    //If NO referrer makes an admin referrer
+    if (!referrer) referrer = await User.findOne({ role: "admin" });
+    let referrerDetails = await UserDetails.findOne({ userId: referrer._id });
+    if (!referrerDetails) {
+      referrer = await User.findOne({ role: "admin" });
+      referrerDetails = await UserDetails.findOne({ userId: referrer._id });
+    }
+    const adminDatas = await Admin.find();
+    const adminData = adminDatas[0];
+
+    // Checks for user details
+    const userDetails = await UserDetails.findOne({ userId: validUser._id });
+    if (userDetails) {
+      const error = ErrorHandler(400, "User's details already exists.");
+      return res.status(400).json(error);
+    }
+
+    // Gets base url and private key
+    const baseUrl =
+      process.env.NODE_ENV !== "production"
+        ? process.env.DEMO_MONICREDIT_API
+        : process.env.LIVE_MONICREDIT_API;
+    const priKey =
+      process.env.NODE_ENV !== "production"
+        ? process.env.DEMO_PRI_KEY
+        : process.env.PROD_PRI_KEY;
+
+    // Creates user data
+    const userData = {
+      private_key: priKey,
+      first_name: req.user.firstname,
+      last_name: req.user.lastname,
+      phone: "0" + req.user.phone,
+      email: req.user.email,
+    };
+
+    // Creates Virtual Account For User
+    const accData = await useExternalApi(
+      `${baseUrl}/payment/virtual-account/create`,
+      "POST",
+      userData,
+      null
+    );
+    if (!accData.status) {
+      return res
+        .status(400)
+        .json({ message: `Creation failed: ${accData.message}` });
+    }
+    const accDetails = accData.data;
+
+    // Creates user details for user
     const newUserDetails = new UserDetails({
       userId: validUser._id,
       location,
@@ -81,17 +114,17 @@ export const addUserDetails = async (req, res, next) => {
       bankDetails,
       userEarnings,
       walletDetails: {
-        customerId: "",
-        walletId: "",
-        customerEmail: "",
-        bankName: "",
-        accountName: "",
-        accountNumber: "",
-        balance: 0,
-        credit: 0,
-        debit: 0,
-        reference: "",
-        virtualAccounts: {},
+        customerId: accDetails.customer_id,
+        walletId: accDetails.wallet_id,
+        customerEmail: accDetails.customer_email,
+        bankName: accDetails.bank_name,
+        accountName: accDetails.account_name,
+        accountNumber: accDetails.account_number,
+        balance: accDetails.balance,
+        credit: accDetails.credit,
+        debit: accDetails.debit,
+        reference: accDetails.reference,
+        virtualAccounts: accDetails.virtual_accounts,
       },
     });
     if (image) {
@@ -150,6 +183,7 @@ export const becomeAMember = async (req, res, next) => {
       const error = ErrorHandler(404, "There's no user with this email.");
       return res.status(404).json(error);
     }
+    let userDetails = await UserDetails.findOne({ userId: req.user._id });
     let referrer = await User.findOne({ username: validUser.referredBy });
     //If NO referrer makes an admin referrer
     if (!referrer) referrer = await User.findOne({ role: "admin" });
@@ -162,86 +196,15 @@ export const becomeAMember = async (req, res, next) => {
     const adminDatas = await Admin.find();
     const adminData = adminDatas[0];
 
-    // Gets base url and private key
-    const baseUrl =
-      process.env.NODE_ENV !== "production"
-        ? process.env.DEMO_MONICREDIT_API
-        : process.env.LIVE_MONICREDIT_API;
-    const priKey =
-      process.env.NODE_ENV !== "production"
-        ? process.env.DEMO_PRI_KEY
-        : process.env.PROD_PRI_KEY;
-
-    // Creates transaction data
-    const transactionData = {
-      transaction_id: req.body.transactionId,
-      private_key: priKey,
-    };
-
-    // creates user data
-    const userData = {
-      private_key: priKey,
-      first_name: req.user.firstname,
-      last_name: req.user.lastname,
-      phone: "0" + req.user.phone,
-      email: req.user.email,
-    };
-
     // Verifies payment
-    const verificationData = await useExternalApi(
-      `${baseUrl}/payment/transactions/verify-payment`,
-      "POST",
-      transactionData,
-      null
-    );
-    if (!verificationData.status) {
-      return res.status(402).json({
-        message: `Payment verification failed: ${verificationData.message}`,
+    const balanceIsNotValid =
+      userDetails.userEarnings.balance < adminData.membershipFee;
+    if (balanceIsNotValid) {
+      return res.status(406).json({
         failed: true,
+        message: "Insufficient balance. Fund your wallet to continue.",
       });
     }
-    const verificationDetails = verificationData.data;
-    if (verificationDetails.amount < adminData.membershipFee) {
-      return res
-        .status(402)
-        .json({ message: "Insufficient balance", failed: true });
-    }
-
-    // Creates Virtual Account For User
-    const accData = await useExternalApi(
-      `${baseUrl}/payment/virtual-account/create`,
-      "POST",
-      userData,
-      null
-    );
-    if (!accData.status) {
-      return res
-        .status(400)
-        .json({ message: `Creation failed: ${accData.message}` });
-    }
-    const accDetails = accData.data;
-
-    // Creates Local User Wallet
-    const walletDetails = {
-      customerId: accDetails.customer_id,
-      walletId: accDetails.wallet_id,
-      customerEmail: accDetails.customer_email,
-      bankName: accDetails.bank_name,
-      accountName: accDetails.account_name,
-      accountNumber: accDetails.account_number,
-      balance: accDetails.balance,
-      credit: accDetails.credit,
-      debit: accDetails.debit,
-      reference: accDetails.reference,
-      virtualAccounts: accDetails.virtual_accounts,
-    };
-    // Updates User wallet details
-    await userDetails.findOneAndUpdate(
-      { userId: req.user._id },
-      {
-        walletDetails: walletDetails,
-      }
-    );
 
     // Updates referrer earning details
     referrerDetails.userEarnings = {
@@ -253,8 +216,7 @@ export const becomeAMember = async (req, res, next) => {
         referrerDetails.userEarnings.totalEarnings +
         0.6 * adminData.membershipFee,
       balance:
-        referrerDetails.userEarnings.totalEarnings +
-        0.6 * adminData.membershipFee,
+        referrerDetails.userEarnings.balance + 0.6 * adminData.membershipFee,
     };
     referrerDetails.walletDetails = {
       ...referrerDetails.walletDetails,
@@ -262,11 +224,22 @@ export const becomeAMember = async (req, res, next) => {
         referrerDetails.walletDetails.balance + 0.6 * adminData.membershipFee,
     };
 
+    // Updates user balance
+    userDetails.userEarnings = {
+      ...userDetails.userEarnings,
+      balance: userDetails.userEarnings.balance - adminData.membershipFee,
+    };
+    userDetails.walletDetails = {
+      ...userDetails.walletDetails,
+      balance: userDetails.walletDetails.balance - adminData.membershipFee,
+    };
+
     // Makes user a member
     await User.findByIdAndUpdate(req.user._id, { isMember: true });
 
-    // Saves referrer details
+    // Saves user and referrer wallet details
     await referrerDetails.save();
+    await userDetails.save();
 
     // Creates notitfication
     const notification = {
