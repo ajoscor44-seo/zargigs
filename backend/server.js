ec771086880ef012daed494e38ff6bc8a16a2f2a
justlogicalahmad <rabiu2993@gmail.com>
2024-06-26 10:20:39 -0700

    Return device restrictions

--- backend/server.js
+++ backend/server.js
@@ -59,7 +59,7 @@ app.options("*", cors(corsOptions));
 
 // Blocks desktop devices
 app.use(useragent.express());
-// app.use(blockDesktopsMiddleware);
+app.use(blockDesktopsMiddleware);
 
 // Rate limits user
 app.use(limiter);
