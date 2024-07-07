0e32cd86e958ee0e57881881d5278ae9705c5ab9
justlogicalahmad <rabiu2993@gmail.com>
2024-07-07 07:58:40 -0700

    Fix: Change manual funding whatsapp link

--- frontend/app/src/components/ManualFunding/ManualFunding.jsx
+++ frontend/app/src/components/ManualFunding/ManualFunding.jsx
@@ -39,7 +39,7 @@ const ManualFunding = () => {
           Contact Support using the chat button below.
         </span>
       </h2>
-      <Chat whatsappLink={"https://wa.link/tfwje6"} />
+      <Chat whatsappLink={"https://wa.link/g5hruc"} />
     </div>
   );
 };
