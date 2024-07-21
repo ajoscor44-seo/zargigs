a0286ff50d8eec76c1958409fbb70636e9fc4874
justlogicalahmad <rabiu2993@gmail.com>
2024-07-21 08:45:50 -0700

    Gigsflix advert banner

--- frontend/app/src/components/Advertisements/Advertisements.jsx
+++ frontend/app/src/components/Advertisements/Advertisements.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import banner from "../../assets/images/gigsflix_advert_banner.png";
 
 const Advertisements = () => {
-  return <div className="border rounded-sm mx-4 px-2 py-3">Advertisements</div>;
+  return (
+    <div className="border rounded-sm mx-4 px-2 py-3">
+      <img
+        className="w-full h-20 object-cover"
+        src={banner}
+        alt="Advert banner"
+      />
+    </div>
+  );
 };
 
 export default Advertisements;
