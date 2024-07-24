7dd747f493c052dea08ba42f8970e3bc58c8defc
justlogicalahmad <rabiu2993@gmail.com>
2024-07-24 08:14:50 -0700

    Add: Building advertisement feature

--- frontend/app/src/components/CreateAdvertisement/CreateAdvertisement.jsx
+++ frontend/app/src/components/CreateAdvertisement/CreateAdvertisement.jsx
@@ -4,25 +4,36 @@ import FormInput from "../FormInput/FormInput";
 import axios from "axios";
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { storage } from "../../config/firebase.config";
+import PayAmountBar from "../PayAmountBar/PayAmountBar";
 
-const CreateAdvertisement = ({
-  advertData,
-  setAdvertData,
-  setCreatingAdvert,
-}) => {
-  const [image, setImage] = useState(null);
+const CreateAdvertisement = ({ setCreatingAdvert }) => {
   const [imagePercentage, setImagePercentage] = useState(0);
   const [imageError, setImageError] = useState(null);
+  const [advertData, setAdvertData] = useState({
+    name: "",
+    description: "",
+    duration: 1,
+    banner: "",
+    link: "",
+  });
+  const amountToPay = advertData.duration * 1500;
+
+  const uploadAdvert = () => {
+    try {
+      const response = axios.post("api/v1", advertData);
+
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const handleChange = (e) => {
+    console.log(advertData);
     const isFile = e.target.type === "file";
 
     if (isFile) {
-      handleFileInputChange(e);
-      return setAdvertData({
-        ...advertData,
-        [e.target.name]: image,
-      });
+      return handleFileInputChange(e);
     }
     return setAdvertData({
       ...advertData,
@@ -33,15 +44,30 @@ const CreateAdvertisement = ({
   // Handles File Input
   const handleFileInputChange = (event) => {
     const file = event.target.files[0];
-    return uploadProfilePic(file);
+    if (!file) {
+      alert("No file chosen.");
+      return;
+    }
+
+    if (file.type.startsWith("image") && file.size > 2097152) {
+      // 2 MB for images
+      return alert("The photo is too large. Maximum size is 2 MB.");
+    } else if (file.type.startsWith("video")) {
+      // No video allowed
+      return alert("Video is not allowed as media type.");
+    } else {
+      alert("File is accepted.");
+      // Handle the file upload process here
+      return uploadMedia(file);
+    }
   };
 
   // Upload Profile Picture
-  const uploadProfilePic = (image) => {
+  const uploadMedia = (image) => {
     setImagePercentage(null);
     setImageError(null);
     const fileName = new Date().getTime() + image.name;
-    const storageRef = ref(storage, "profile_pics/" + fileName);
+    const storageRef = ref(storage, "advertisments/" + fileName);
     const uploadTask = uploadBytesResumable(storageRef, image);
 
     // Returns the progress of the image
@@ -59,8 +85,12 @@ const CreateAdvertisement = ({
       },
       () => {
         getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
-          setImage(downloadUrl);
           setImageError(null);
+          console.log(downloadUrl);
+          return setAdvertData({
+            ...advertData,
+            banner: downloadUrl,
+          });
         });
       }
     );
@@ -78,56 +108,65 @@ const CreateAdvertisement = ({
   };
 
   return (
-    <div className="shadow-2xl border flex flex-col w-full px-5 gap-3 py-5">
-      <div className="flex items-center justify-between">
-        <h2 className="font-bold">Advert Setup</h2>
-        <button
-          className="border rounded-full p-1"
-          onClick={() => setCreatingAdvert(false)}
-        >
-          <IoClose size={20} className="text-red-500" />
-        </button>
+    <div>
+      <div className="shadow-2xl border flex flex-col w-full px-5 gap-3 py-5">
+        <div className="flex items-center justify-between">
+          <h2 className="font-bold">Advert Setup</h2>
+          <button
+            className="border rounded-full p-1"
+            onClick={() => setCreatingAdvert(false)}
+          >
+            <IoClose size={20} className="text-red-500" />
+          </button>
+        </div>
+        <FormInput
+          label={"Name Your Advert"}
+          placeholder={"Advert Name"}
+          isError={false}
+          type={"text"}
+          name={"name"}
+          handleChange={handleChange}
+        />
+        <FormInput
+          label={"Advert Link"}
+          placeholder={"https://www.example.com/link-to-advert"}
+          isError={false}
+          type={"text"}
+          name={"link"}
+          handleChange={handleChange}
+        />
+        <FormInput
+          label={"Description"}
+          placeholder={"Describe Your Advert"}
+          isError={false}
+          type={"text"}
+          name={"description"}
+          handleChange={handleChange}
+          useTextArea={true}
+        />
+        <FormInput
+          label={"Upload Banner"}
+          placeholder={"Describe Your Advert"}
+          isError={false}
+          type={"file"}
+          name={"banner"}
+          handleChange={handleChange}
+        />
+        <FormInput
+          label={"Duration (in Days)"}
+          placeholder={"Number of days you want your advert to use on gigsflix"}
+          isError={false}
+          type={"number"}
+          name={"duration"}
+          handleChange={handleChange}
+        />
       </div>
-      <FormInput
-        label={"Name Your Advert"}
-        placeholder={"Advert Name"}
-        isError={false}
-        type={"text"}
-        name={"name"}
-        handleChange={handleChange}
-      />
-      <FormInput
-        label={"Advert Link"}
-        placeholder={"https://www.example.com/link-to-advert"}
-        isError={false}
-        type={"text"}
-        name={"link"}
-        handleChange={handleChange}
-      />
-      <FormInput
-        label={"Description"}
-        placeholder={"Describe Your Advert"}
-        isError={false}
-        type={"text"}
-        name={"description"}
-        handleChange={handleChange}
-        useTextArea={true}
-      />
-      <FormInput
-        label={"Upload Banner"}
-        placeholder={"Describe Your Advert"}
-        isError={false}
-        type={"file"}
-        name={"banner"}
-        handleChange={handleChange}
-      />
-      <FormInput
-        label={"Duration (in Days)"}
-        placeholder={"Number of days you want your advert to use on gigsflix"}
-        isError={false}
-        type={"number"}
-        name={"duration"}
-        handleChange={handleChange}
+      <PayAmountBar
+        feeTitle={"Advertisement Fee:"}
+        fee={amountToPay}
+        disable={true}
+        btnText={"Pay For Advert"}
+        handleClick={uploadAdvert}
       />
     </div>
   );
--- frontend/app/src/pages/Advertisement.jsx
+++ frontend/app/src/pages/Advertisement.jsx
@@ -1,50 +1,23 @@
 import React, { useState } from "react";
 import BackNav from "../components/BackNav/BackNav";
-import PayAmountBar from "../components/PayAmountBar/PayAmountBar";
 import ClientMenuBar from "../components/ClientMenuBar/ClientMenuBar";
 import AdvertisementList from "../components/AdvertisementList/AdvertisementList";
 import CreateAdvertisement from "../components/CreateAdvertisement/CreateAdvertisement";
 
 const Advertisement = () => {
   const [creatingAdvert, setCreatingAdvert] = useState(false);
-  const [advertData, setAdvertData] = useState({
-    name: "",
-    description: "",
-    duration: 1,
-    banner: "",
-    link: "",
-  });
-  const amountToPay = advertData.duration * 1500;
 
-  const uploadAdvert = () => {
-    try {
-      //
-    } catch (error) {
-      console.error(error);
-    }
-  };
   return (
     <div>
       <BackNav pageName={"Manage Advertisement"} />
       <div className="underBackNav flex flex-col justify-center items-center px-2 mb-32">
         <h1 className="mt-4 mb-2 font-bold text-xl">Post Advert On Gigsflix</h1>
         {creatingAdvert ? (
-          <CreateAdvertisement
-            advertData={advertData}
-            setAdvertData={setAdvertData}
-            setCreatingAdvert={setCreatingAdvert}
-          />
+          <CreateAdvertisement setCreatingAdvert={setCreatingAdvert} />
         ) : (
           <AdvertisementList setCreatingAdvert={setCreatingAdvert} />
         )}
       </div>
-      <PayAmountBar
-        feeTitle={"Advertisement Fee:"}
-        fee={amountToPay}
-        disable={true}
-        btnText={"Pay For Advert"}
-        handleClick={uploadAdvert}
-      />
       <ClientMenuBar />
     </div>
   );
