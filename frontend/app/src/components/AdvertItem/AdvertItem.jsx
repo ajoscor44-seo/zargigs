5b360390d104580a183953700bd8520836bd5c09
justlogicalahmad <rabiu2993@gmail.com>
2024-08-03 14:57:18 -0700

    Fix: Advertisement lists UI

--- frontend/app/src/components/AdvertItem/AdvertItem.jsx
+++ frontend/app/src/components/AdvertItem/AdvertItem.jsx
@@ -8,23 +8,36 @@ const AdvertItem = ({ itemData }) => {
         className="w-full h-48 object-cover border-b"
         src={itemData.banner}
       />
-      <div className="px-2 flex flex-col gap-1 my-1">
-        <h2 className="font-semibold">
-          Name: <span className="text-green-500">{itemData.name}</span>
-        </h2>
-        <p className="font-semibold">
-          Contact: <span className="text-green-500">{itemData.link}</span>
-        </p>
-        <p className="font-semibold">
-          Description:{" "}
-          <span className="text-green-500">{itemData.description}</span>
-        </p>
-        <CountdownTimer
-          totalSeconds={Math.floor(
-            (new Date(itemData.expiresAt).getTime() - new Date().getTime()) /
-              1000
-          )}
-        />
+      <div>
+        <div className="px-2 flex flex-col gap-1 my-1">
+          <div className="flex justify-between">
+            <div>
+              <h2 className="font-semibold">
+                Name: <span className="text-green-500">{itemData.name}</span>
+              </h2>
+              <p className="font-semibold">
+                Contact: <span className="text-green-500">{itemData.link}</span>
+              </p>
+              <p className="font-semibold">
+                Description:{" "}
+                <span className="text-green-500">{itemData.description}</span>
+              </p>
+            </div>
+            <div>
+              <p>Duration: </p>
+              <h2 className="border text-center py-2 text-2xl font-bold text-green-500">
+                {itemData.duration} <span className="text-xs">days</span>
+              </h2>
+            </div>
+          </div>
+          <p>Remaining:</p>
+          <CountdownTimer
+            totalSeconds={Math.floor(
+              (new Date(itemData.expiresAt).getTime() - new Date().getTime()) /
+                1000
+            )}
+          />
+        </div>
       </div>
     </div>
   );
--- frontend/app/src/components/AdvertisementList/AdvertisementList.jsx
+++ frontend/app/src/components/AdvertisementList/AdvertisementList.jsx
@@ -11,7 +11,7 @@ const AdvertisementList = ({ setCreatingAdvert }) => {
 
   const getAdvertisements = async () => {
     try {
-      const response = await axios.get("/api/v1/advertisements");
+      const response = await axios.get("/api/v1/advertisements/user");
 
       setAdvertisementList(response.data.data);
       return setLoading(false);
