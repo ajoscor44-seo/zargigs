70f509084082c40bc9ded4001f277f1052c7e48b
Rabiu Ahmad O. <rabiu2993@gmail.com>
2024-10-21 22:25:54 +0100

    Fix: Removed login to monicredit on user login

--- backend/api/V1/Controllers/auth.controller.js
+++ backend/api/V1/Controllers/auth.controller.js
@@ -124,34 +124,6 @@ export const login = async (req, res, next) => {
     const { password: hashedPassword, referrals, ...rest } = validUser._doc;
     const token = jwt.sign({ ...rest }, process.env.JWT_SECRET);
 
-    const baseUrl =
-      process.env.NODE_ENV !== "production"
-        ? process.env.DEMO_MONICREDIT_API
-        : process.env.LIVE_MONICREDIT_API;
-
-    const cred = {
-      email: process.env.USER,
-      password: process.env.PASSWORD,
-    };
-    const authRes = await useExternalApi(
-      `${baseUrl}/core/auth/login`,
-      "POST",
-      cred
-    );
-
-    if (!authRes.success) {
-      return next();
-    }
-    await AccessToken.findOneAndUpdate(
-      { username: validUser.username },
-      {
-        username: validUser.username,
-        email,
-        accessToken: authRes.accessToken,
-      },
-      { new: true, upsert: true }
-    );
-
     return res
       .cookie("access_token", token, {
         httpOnly: true,
