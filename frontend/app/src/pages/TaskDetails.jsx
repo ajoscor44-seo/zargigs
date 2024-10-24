f5a9718fbb378979e61dbf9ccf5173a4d35ba408
Rabiu Ahmad O. <rabiu2993@gmail.com>
2024-10-24 10:01:16 +0100

    Fix hidden upload UI

--- frontend/app/src/pages/TaskDetails.jsx
+++ frontend/app/src/pages/TaskDetails.jsx
@@ -463,10 +463,10 @@ const TaskDetails = () => {
                         </p>
                       )}
                     </p>
-                    <p>
+                    {/* <p>
                       {taskDetails?.description &&
                         "Poster said: " + taskDetails?.description}
-                    </p>
+                    </p> */}
                   </div>
                   <div className="text-orange-400 font-semibold bg-orange-100 rounded text-xs p-3 mt-2">
                     You must{" "}
